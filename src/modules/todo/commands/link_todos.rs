365298abf6a02e8a7c2839ea0825f24c03b28ec8
drillfreak100 <blusky_8893@hotmail.com>
2023-01-12 09:46:17 +0800

    refactor: optimize linking logics

--- src/modules/todo/commands/link_todos.rs
+++ src/modules/todo/commands/link_todos.rs
@@ -1,9 +1,11 @@
+use std::collections::HashSet;
+
 use crate::{
     generated::schema::todos_relations::{self, child_todo_id, parent_todo_id, relationship_type},
-    modules::todo::models::LinkTodosRequest,
-    utils::{db::*, http::*},
+    modules::todo::models::{LinkTodosRequest, TodoRelation},
+    utils::db::*,
 };
-use actix_web::{put, web, Responder};
+use actix_web::{put, web, HttpResponse, Responder};
 use diesel::prelude::*;
 
 /// Link todos to a todo
@@ -25,23 +27,48 @@ pub async fn link_todos(
     let req_body = &req_body.into_inner();
 
     db_pool.run(|con| {
-        diesel::delete(todos_relations::table.filter(parent_todo_id.eq(id)))
+        let mut existing_link_ids: HashSet<i32> = todos_relations::table
+            .filter(parent_todo_id.eq(id))
+            .load::<TodoRelation>(con)
+            .expect("Error fetching existing links")
+            .into_iter()
+            .map(|e| e.child_todo_id.unwrap())
+            .collect();
+
+        let mut to_adds = vec![];
+
+        for id in req_body.todo_ids.clone() {
+            if !existing_link_ids.contains(&id) {
+                to_adds.push(id);
+                continue;
+            }
+
+            // Remove id to persist. Leftovers will be deleted
+            existing_link_ids.remove(&id);
+        }
+
+        // Delete unlink todos
+        diesel::delete(todos_relations::table.filter(child_todo_id.eq_any(existing_link_ids)))
             .execute(con)
             .expect("Error deleting todo links");
 
-        let mut data = vec![];
+        // Insert new links
+        if !to_adds.is_empty() {
+            let mut data = vec![];
+            for todo_id in to_adds {
+                data.push((
+                    parent_todo_id.eq(id),
+                    child_todo_id.eq(todo_id),
+                    relationship_type.eq(req_body.relationship_type),
+                ));
+            }
 
-        for todo_id in req_body.todo_ids.iter() {
-            data.push((
-                parent_todo_id.eq(id),
-                child_todo_id.eq(*todo_id),
-                relationship_type.eq(req_body.relationship_type),
-            ));
+            diesel::insert_into(todos_relations::table)
+                .values(&data)
+                .execute(con)
+                .expect("Error linking todos");
         }
 
-        diesel::insert_into(todos_relations::table)
-            .values(&data)
-            .execute(con)
-            .into_affected_res("Error linking todos")
+        Ok(HttpResponse::Ok())
     })
 }
